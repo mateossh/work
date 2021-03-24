bee8b9dd82d80918556628c4aca8d4f5f8521f8c
Mateusz Ż <thedead4fun@live.com>
2021-03-24 09:03:03 +0100

    Change "open_pdf" behavior

--- open_pdf.ps1
+++ open_pdf.ps1
@@ -1,9 +1,13 @@
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.1#parameters
 # https://adamtheautomator.com/powershell-escape-double-quotes/
+# https://stackoverflow.com/questions/4146635/custom-sorting-in-powershell
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/sort-object?view=powershell-7.1
+# (example 8)
 
 $path = Read-Host "Enter path"
+$name_part1 = { if ($_.Name -match '^(\d+)') { [int]$matches[1] } }
 
-Get-ChildItem "$path" -Filter "*.pdf" | Foreach-Object {
+Get-ChildItem "$path" -Filter "*.pdf" | Sort-Object $name_part1 | Foreach-Object {
     $name = $_.FullName
 
     Start-Process 'chrome' -ArgumentList "--incognito","`"$name`""
