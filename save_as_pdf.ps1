f1eaaeec111c5063a3c001ca073dc23862b32f59
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-03-15 11:38:55 +0100

    Update save_as_pdf
    Now scripts looks for doc/docx/odt files

--- save_as_pdf.ps1
+++ save_as_pdf.ps1
@@ -1,4 +1,5 @@
 ﻿# https://stackoverflow.com/questions/16534292/basic-powershell-batch-convert-word-docx-to-pdf
+#https://superuser.com/questions/318197/how-do-i-get-get-childitem-to-filter-on-multiple-file-types
 #
 # save this file with "UTF-8 with BOM"
 
@@ -8,8 +9,7 @@ $dest_path = $cwd + "\uchwały - pdf"
 
 $word_app = New-Object -ComObject Word.Application
 
-# This filter will find .doc as well as .docx documents
-Get-ChildItem -Path $source_path -Filter *.doc? | ForEach-Object {
+Get-ChildItem -Path $source_path | Where-Object { $_.extension -in ".doc",".docx",".odt" } | Sort-Object { [int]$_.Name.Split(' ')[0] } | ForEach-Object {
   Write-Output $_.BaseName
 
   $pdf_filename = "$dest_path\$($_.BaseName).pdf"
@@ -17,7 +17,6 @@ Get-ChildItem -Path $source_path -Filter *.doc? | ForEach-Object {
   $document = $word_app.Documents.Open($_.FullName)
   $document.SaveAs([ref] $pdf_filename, [ref] 17)
   $document.Close()
-
 }
 
 $word_app.Quit()
