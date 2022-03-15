9938bb1b9801d4a9ee77f2279b1e5753067e17d1
Mateusz Ż <zochowski.mateusz@outlook.com>
2022-03-15 11:39:27 +0100

    Add pdf -> jpg script

---
+++ convert_pdf_to_jpg.ps1
@@ -0,0 +1,24 @@
+﻿# convert_pdf_to_jpg
+#
+# add NAPS2 installation folder to PATH
+#
+# save this file with "UTF-8 with BOM"
+#
+# running "naps2.console -i file.pdf -o file.jpg" runs scanning process
+# with default profile :thinking:
+
+$cwd = (Get-Item .).FullName
+$source_path = $cwd + "\uchwały - pdf"
+$dest_path = $cwd + "\uchwały - jpg"
+$counter = 1
+
+Get-ChildItem -Path $source_path -Filter *.pdf | Sort-Object { [int]$_.Name.Split(' ')[0] } | ForEach-Object {
+  Write-Output $_.BaseName
+
+  $jpg_filename = "$dest_path\$($counter).jpg"
+  $qwer = $_.FullName
+  
+  naps2.console -i "`"$qwer`"" -o "`"$jpg_filename`"" -n 0
+
+  $counter += 1
+}
